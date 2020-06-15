e8397f5eb0c5cc0fbd09d77ef5175a8f971e6c57
remah massri <remah.massri@gmail.com>
2020-06-15 09:54:09 +0300

    add copyriht

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 'use strict';
-
 //Global Variables
-
 //Global Variable - Store hours
 var hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 //Global Variable - Store Locations Array
